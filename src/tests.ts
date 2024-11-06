4e8c37bf45142568d78ab91826fb8180dbb26775
JotaPe36043 <137961559+JotaPe36043@users.noreply.github.com>
2024-11-06 18:28:10 -0300

    Update tests.ts

--- src/tests.ts
+++ src/tests.ts
@@ -1,22 +1,27 @@
 import * as Gi from "react-icons/gi";
 import * as Ci from "react-icons/ci";
 
+import Ex1 from './img/Exercicios/Ex1.png'
+import Ex2 from './img/Exercicios/Ex2.png'
+import Ex3 from './img/Exercicios/Ex3.png'
+import Ex4 from './img/Exercicios/Ex4.png'
+
 export const exercicios = [
     {
         nome:"Rosca Direta",
-        img:"./src/img/Exercicios/Ex1.png"
+        img:Ex1
     },
     {
         nome:"Agachamento Livre",
-        img:"./src/img/Exercicios/Ex2.png"
+        img:Ex2
     },
     {
         nome:"Supino com Halteres",
-        img:"./src/img/Exercicios/Ex3.png"
+        img:Ex3
     },
     {
         nome:"Abdominal com Anilha",
-        img:"./src/img/Exercicios/Ex4.png"
+        img:Ex4
     }
 ]
 
